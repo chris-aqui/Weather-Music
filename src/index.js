7784dfcd030c17d1dbaa97b4dd37ec47479e4fe0
Christine Aqui <2739361+christine-aqui@users.noreply.github.com>
2018-03-07 13:24:44 -0500

    Some setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "react-dom"; // only importing the render method here
+// Components
+import App from './components/App';
+
+// Style
+
+render(<App />, document.querySelector('#main'));
